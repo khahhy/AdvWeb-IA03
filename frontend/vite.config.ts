5d1979787d1077bb2fc7e74fc0fa4fd9443dc71a
khahhy <127372724+khahhy@users.noreply.github.com>
2025-11-06 23:13:50 +0700

    fix(frontend): fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  preview: {
+    host: true,
+    port: parseInt(process.env.PORT || "") || 5173,
+    allowedHosts: true,
+  },
 });
